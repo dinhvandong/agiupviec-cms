6e622fc63ee02a6e79d66cebca23695b9589d8e6
dinhvandong <dongvan.ict3@gmail.com>
2024-01-17 16:36:19 +0700

    Fix
    Fix code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import generateRoutes from './generateRoutes';
-import routesData from './routes.json';
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import PrivateRoute from './components/common/PrivateRoute';
 
 const App = () => {
-  const routes = routesData.routes;
-
+  // const routes = routesData.routes;
+  const isAuthenticated = false;
   return (
     <BrowserRouter>
-      {generateRoutes(routes)}
+      <Routes>
+        <Route path="/" element={<Login/>} />
+        <Route
+          path="/dashboard"
+          element={<PrivateRoute component={Dashboard} isAuthenticated={isAuthenticated} />}
+        />
+      </Routes>
     </BrowserRouter>
   );
 };
--- src/index.js
+++ src/index.js
@@ -5,19 +5,19 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AppStateProvider } from './AppStateContext';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <AppStateProvider>
-//     <App />
-//   </AppStateProvider>,
-// );
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <AppStateProvider>
     <App />
   </AppStateProvider>,
-  document.getElementById('root')
 );
+
+// ReactDOM.render(
+//   <AppStateProvider>
+//     <App />
+//   </AppStateProvider>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
